20ef2b52a28182a2ebd925eb414447c555afad04
Sahar Mor <saharhashai@gmail.com>
2024-11-06 13:58:50 -0800

    Remove verbose

--- llm_testing/example_test.py
+++ llm_testing/example_test.py
@@ -3,5 +3,5 @@ from run_tests import generate_test_results_report, run_tests
 
 if __name__ == "__main__":
     load_dotenv()
-    evaluation_results = run_tests(print_verbose=True)
+    evaluation_results = run_tests()
     generate_test_results_report(evaluation_results)
