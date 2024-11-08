3f9d37cacd560753dde3fc2eb92c8ddf2db8bf09
Sahar Mor <saharhashai@gmail.com>
2024-11-08 13:22:17 -0800

    Add report example

--- llm_testing/example_test.py
+++ llm_testing/example_test.py
@@ -4,6 +4,6 @@ from utils.generate_report import generate_test_results_report
 
 if __name__ == "__main__":
     load_dotenv()
-    test_result = run_tests(tests_to_run=["jailbreaking_airline_agent"])
-    # test_result = run_tests()
+    # test_result = run_tests(tests_to_run=["jailbreaking_airline_agent"])
+    test_result = run_tests()
     generate_test_results_report(test_result)
