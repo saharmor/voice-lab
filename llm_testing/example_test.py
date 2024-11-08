187b460463029ddcc019051864b30d3d6654bc8a
Sahar Mor <saharhashai@gmail.com>
2024-11-08 13:07:33 -0800

    Break down the monolith

--- llm_testing/example_test.py
+++ llm_testing/example_test.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
-from run_tests import generate_test_results_report, run_tests
+from run_tests import run_tests
+from utils.generate_report import generate_test_results_report
 
 if __name__ == "__main__":
     load_dotenv()
     test_result = run_tests(tests_to_run=["jailbreaking_airline_agent"])
+    # test_result = run_tests()
     generate_test_results_report(test_result)
