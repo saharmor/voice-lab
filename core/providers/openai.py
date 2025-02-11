32a3dc0f77b3d3bef93dea50933beec0885299ce
Sahar Mor <saharhashai@gmail.com>
2025-02-11 15:37:03 +0200

    VQA using openai

--- core/providers/openai.py
+++ core/providers/openai.py
@@ -1,3 +1,4 @@
+import base64
 from typing import Any, Dict, List, Optional
 import openai
 from pydantic import BaseModel
@@ -52,4 +53,33 @@ class OpenAIProvider(LLMInterface):
             return LLMResponse(response_msg.content, response_msg.tool_calls)
         except Exception as e:
             raise Exception(f"OpenAI API error: {str(e)}")
-            
+    
+    async def analyze_image(self, image_path: str, prompt: str, response_format: BaseModel) -> str:
+        def encode_image(image_path):
+            with open(image_path, "rb") as image_file:
+                return base64.b64encode(image_file.read()).decode("utf-8")
+
+        base64_image = encode_image(image_path)
+        chat_completion = self.client.beta.chat.completions.parse(
+            model=self.model,
+            messages=[{
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": prompt
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                        },
+                    ],
+                }
+            ],               
+            response_format=response_format,
+            )
+
+        return chat_completion.choices[0].message.parsed
+    
+
+      
